4d9eca11264e1938567e995f8ea6d4e86753d0c8
pmayur <thisispatil@gmail.com>
2020-12-12 00:29:53 +0530

    add: new error in errors

--- util/errors.js
+++ util/errors.js
@@ -1,6 +1,7 @@
 const ERR = {
     INCOMPLETE_INPUT    : "incomplete input",
     INVALID_INPUT       : "invalid input",
+    INVALID_ID          : "invalid id provided",
 }
 
 module.exports = { ERR }
